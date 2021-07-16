f17c042917bd66837e7be5d747a7ed734ab4198b
NeoZit <fedotov2189@mail.ru>
2021-07-16 14:50:44 +0300

    added shop-grid page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,23 @@
 $(function() {
+
+    $(".filter-products__star").rateYo({
+        rating: 4,
+        starWidth: "10px"
+    });
+
+    $(".filter-price__input").ionRangeSlider({
+
+        onStart: function(data) {
+            $('.filter-price__from').text(data.from);
+            $('.filter-price__to').text(data.to);
+        },
+        onChange: function(data) {
+            $('.filter-price__from').text(data.from);
+            $('.filter-price__to').text(data.to);
+        }
+
+    });
+
     $('.top-slider__inner').slick({
         arrows: false,
         dots: true,
