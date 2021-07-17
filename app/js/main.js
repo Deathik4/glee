6c219948bb0e132c44b4576486bf76f7e30800f2
NeoZit <fedotov2189@mail.ru>
2021-07-17 14:23:30 +0300

    added shop-list page

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,22 @@
 $(function() {
 
+    $('.products__item-inner--cart').on('click', function() {
+        $('.products__item-btn').addClass('products__item-btn--active');
+        $('.products__item-inner').addClass('products__item-inner--active');
+    });
+
+    $('.products__item-btn').on('click', function() {
+        $('.products__item-btn').removeClass('products__item-btn--active');
+        $('.products__item-inner').removeClass('products__item-inner--active');
+    });
+
     $(".filter-products__star").rateYo({
         rating: 4,
         starWidth: "10px"
+    });
+    $(".products__item-star").rateYo({
+        rating: 4,
+        starWidth: "18px"
     });
 
     $(".filter-price__input").ionRangeSlider({
